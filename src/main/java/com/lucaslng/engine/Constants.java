de95ff81c30877c42ba1a2885a841388aa37645f
lucaslng <saculgnuel1@gmail.com>
2025-02-22 18:28:05 -0500

    move resources

--- src/main/java/com/lucaslng/engine/Constants.java
+++ src/main/java/com/lucaslng/engine/Constants.java
@@ -2,7 +2,7 @@ package com.lucaslng.engine;
 
 public class Constants {
 
-	public static final String RESOURCES_DIR = "resources/";
+	public static final String RESOURCES_DIR = "src/main/resources/";
 	public static final String SHADERS_DIR = RESOURCES_DIR + "shaders/";
 	public static final String TEXTURES_DIR = RESOURCES_DIR + "textures/";
 	
