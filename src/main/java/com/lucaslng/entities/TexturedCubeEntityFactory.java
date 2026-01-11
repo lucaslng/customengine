2120f35b95048ab97c0c06be5ef62fb3cf908b3d
lucaslng <saculgnuel1@gmail.com>
2026-01-11 01:30:55 -0500

    shorten textured cube

--- src/main/java/com/lucaslng/entities/TexturedCubeEntityFactory.java
+++ src/main/java/com/lucaslng/entities/TexturedCubeEntityFactory.java
@@ -7,125 +7,67 @@ import com.lucaslng.engine.entities.AbstractEntityFactory;
 
 public class TexturedCubeEntityFactory implements AbstractEntityFactory {
 
-	private static final int[] indices = new int[36];
-	private final float[] vertices;
-	private final Vector3f position;
-	private final float size;
-
-	private static final float[] uvs = new float[] {
-			0.0f, 0.0f,
-			1.0f, 0.0f,
-			1.0f, 1.0f,
-			1.0f, 1.0f,
-			0.0f, 1.0f,
-			0.0f, 0.0f,
-
-			0.0f, 0.0f,
-			1.0f, 0.0f,
-			1.0f, 1.0f,
-			1.0f, 1.0f,
-			0.0f, 1.0f,
-			0.0f, 0.0f,
-
-			1.0f, 0.0f,
-			1.0f, 1.0f,
-			0.0f, 1.0f,
-			0.0f, 1.0f,
-			0.0f, 0.0f,
-			1.0f, 0.0f,
-
-			1.0f, 0.0f,
-			1.0f, 1.0f,
-			0.0f, 1.0f,
-			0.0f, 1.0f,
-			0.0f, 0.0f,
-			1.0f, 0.0f,
-
-			0.0f, 1.0f,
-			1.0f, 1.0f,
-			1.0f, 0.0f,
-			1.0f, 0.0f,
-			0.0f, 0.0f,
-			0.0f, 1.0f,
-
-			0.0f, 1.0f,
-			1.0f, 1.0f,
-			1.0f, 0.0f,
-			1.0f, 0.0f,
-			0.0f, 0.0f,
-			0.0f, 1.0f
+	private static final int[] indices = {
+			0, 1, 2, 2, 3, 0, // front
+			4, 5, 6, 6, 7, 4, // back
+			8, 9, 10, 10, 11, 8, // left
+			12, 13, 14, 14, 15, 12, // right
+			16, 17, 18, 18, 19, 16, // bottom
+			20, 21, 22, 22, 23, 20 // top
 	};
 
-	static {
-		for (int i = 0; i < 36; i++) {
-			indices[i] = i;
-		}
-	}
+	private final float[] vertices;
+	private final Vector3f position;
+	private final float s;
 
 	public TexturedCubeEntityFactory(float x, float y, float z, float size) {
 		position = new Vector3f(x, y, z);
-		this.size = size;
-		float[] positions = new float[] {
-				-size, -size, -size,
-				size, -size, -size,
-				size, size, -size,
-				size, size, -size,
-				-size, size, -size,
-				-size, -size, -size,
+		this.s = size;
 
-				-size, -size, size,
-				size, -size, size,
-				size, size, size,
-				size, size, size,
-				-size, size, size,
-				-size, -size, size,
+		// 24 vertices: position + uv
+		vertices = new float[] {
+				// Front
+				-s, -s, s, 0, 0,
+				s, -s, s, 1, 0,
+				s, s, s, 1, 1,
+				-s, s, s, 0, 1,
 
-				-size, size, size,
-				-size, size, -size,
-				-size, -size, -size,
-				-size, -size, -size,
-				-size, -size, size,
-				-size, size, size,
+				// Back
+				s, -s, -s, 0, 0,
+				-s, -s, -s, 1, 0,
+				-s, s, -s, 1, 1,
+				s, s, -s, 0, 1,
 
-				size, size, size,
-				size, size, -size,
-				size, -size, -size,
-				size, -size, -size,
-				size, -size, size,
-				size, size, size,
+				// Left
+				-s, -s, -s, 0, 0,
+				-s, -s, s, 1, 0,
+				-s, s, s, 1, 1,
+				-s, s, -s, 0, 1,
 
-				-size, -size, -size,
-				size, -size, -size,
-				size, -size, size,
-				size, -size, size,
-				-size, -size, size,
-				-size, -size, -size,
+				// Right
+				s, -s, s, 0, 0,
+				s, -s, -s, 1, 0,
+				s, s, -s, 1, 1,
+				s, s, s, 0, 1,
 
-				-size, size, -size,
-				size, size, -size,
-				size, size, size,
-				size, size, size,
-				-size, size, size,
-				-size, size, -size
+				// Bottom
+				-s, -s, -s, 0, 0,
+				s, -s, -s, 1, 0,
+				s, -s, s, 1, 1,
+				-s, -s, s, 0, 1,
+
+				// Top
+				-s, s, s, 0, 0,
+				s, s, s, 1, 0,
+				s, s, -s, 1, 1,
+				-s, s, -s, 0, 1
 		};
-
-		vertices = new float[positions.length + uvs.length];
-		int p = 0, u = 0;
-		for (int i = 0; i < vertices.length; i += 5) {
-			vertices[i] = positions[p];
-			vertices[i + 1] = positions[p + 1];
-			vertices[i + 2] = positions[p + 2];
-			vertices[i + 3] = uvs[u];
-			vertices[i + 4] = uvs[u + 1];
-			p += 3;
-			u += 2;
-		}
 	}
 
 	@Override
 	public Object[] components() {
 		return new Object[] { new PositionComponent(position), new RotationComponent(new Vector3f()),
-				new RigidBodyComponent(0f, 1f, 1f, 1f), new AABBComponent(new Vector3f(size)),
+				new RigidBodyComponent(0f, 1f, 1f, 1f), new AABBComponent(new Vector3f(s)),
 				new MeshComponent(new SubMesh[] { new SubMesh(vertices, indices, "Cat") }) };
 	};
 }
