333ca5ae47fabc8b961e392345f0bdc4906f5013
lucaslng <saculgnuel1@gmail.com>
2026-01-11 19:51:59 -0500

    better lighting

--- src/main/resources/shaders/fragment.glsl
+++ src/main/resources/shaders/fragment.glsl
@@ -18,13 +18,13 @@ void main() {
     }
 
     // lighting
-    vec3 lightColor = vec3(0.95);
+    vec3 lightColor = vec3(1.0);
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
 
-    vec3 ambient = 0.95 * lightColor; // ambient strength * ambient light color
+    vec3 ambient = 1.0 * lightColor; // ambient strength
 
     vec3 result = (ambient + diffuse) * objectColor.xyz;
     FragColor = vec4(result, objectColor.w);
