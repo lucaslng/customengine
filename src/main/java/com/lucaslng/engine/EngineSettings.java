ee2f2d966a0338e3909b175ea79222c967d589c6
lucaslng <saculgnuel1@gmail.com>
2026-01-12 22:11:30 -0500

    Revert "make enginesettinsg values static and final"
    This reverts commit 700fdd4664dce684efea114f5d9458f2053ad9b5.

--- src/main/java/com/lucaslng/engine/EngineSettings.java
+++ src/main/java/com/lucaslng/engine/EngineSettings.java
@@ -3,13 +3,13 @@ package com.lucaslng.engine;
 import java.awt.Dimension;
 
 public class EngineSettings {
-	public static final String title = "Custom Engine";
-	public static final Dimension windowSize = new Dimension(800, 600);
+	public String title = "Custom Engine";
+	public Dimension windowSize = new Dimension(800, 600);
 
-	public static final float FOV = (float) Math.toRadians(90.0f);
+	public float FOV = (float) Math.toRadians(90.0f);
 
-	public static final float Z_FAR = 100.f;
+	public float Z_FAR = 100.f;
 
-	public static final float sensitivity = 0.01f;
+	public float sensitivity = 0.01f;
 
 }
