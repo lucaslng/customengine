e2eb3d8f1a03894dbef1c1a2593fa94f70f30a1d
lucaslng <saculgnuel1@gmail.com>
2026-01-18 18:07:03 -0500

    remove unused imports

--- src/main/java/com/lucaslng/engine/utils/FileReader.java
+++ src/main/java/com/lucaslng/engine/utils/FileReader.java
@@ -4,11 +4,6 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.List;
-
 import javax.imageio.ImageIO;
 
 public class FileReader {
