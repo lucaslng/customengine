c92609c34fdb42ab4f59437cf8e0fa530e65f8b4
lucaslng <saculgnuel1@gmail.com>
2026-01-18 21:25:35 -0500

    buttons grow when hovered

--- src/main/java/com/lucaslng/engine/ui/Button.java
+++ src/main/java/com/lucaslng/engine/ui/Button.java
@@ -10,8 +10,16 @@ public class Button extends ColoredRectElement {
 	private final ArrayList<Operation> operations;
 	public boolean hovered;
 
+	private static final float hoverScale = 1.05f;
+
+	public float initX, initY, initWidth, initHeight;
+
 	public Button(float x, float y, float width, float height, XAlignment xAlignment, YAlignment yAlignment, Color color) {
 		super(x, y, width, height, xAlignment, yAlignment, color);
+		this.initX = x;
+		this.initY = y;
+		this.initWidth = width;
+		this.initHeight = height;
 		operations = new ArrayList<>();
 		hovered = false;
 	}
@@ -29,12 +37,22 @@ public class Button extends ColoredRectElement {
 	public void onHovered() {
 		if (!hovered) {
 			hovered = true;
+			width *= hoverScale;
+			height *= hoverScale;
+			if (xAlignment != XAlignment.CENTER)
+				x -= (width - initWidth) / 2f;
+			if (yAlignment != YAlignment.CENTER)
+				y -= (height - initHeight) / 2f;
 		}
 	}
 	
 	public void onNotHovered() {
 		if (hovered) {
 			hovered = false;
+			x = initX;
+			y = initY;
+			width = initWidth;
+			height = initHeight;
 		}
 	}
 }
